dfb47e0e190056dd4592541f642f06fc9ab1e037
Laddipeerla <s528805@nwmissouri.edu>
2018-06-17 10:29:56 -0500

    second commit

--- src/excelops/Excelcreator.java
+++ src/excelops/Excelcreator.java
@@ -19,41 +19,36 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
 public class Excelcreator {
 
-	public static void main(String args[]) throws IOException {
-		
+	public static void main(String args[]) throws IOException, illegalSheetIndexException {
+
 		XSSFWorkbook wb = new XSSFWorkbook();
 		XSSFSheet sheet = wb.createSheet("mysheet");
 		XSSFRow row = sheet.createRow(0);
 		XSSFCell cell = row.createCell(0);
 		cell.setCellValue("1. cell");
-		
+
 		cell = row.createCell(1);
 		DataFormat format = wb.createDataFormat();
 		CellStyle dtStyle = wb.createCellStyle();
 		dtStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
 		cell.setCellStyle(dtStyle);
 		cell.setCellValue(new Date());
-		
+
 		row.createCell(2).setCellValue("karthik");
-		
+
 		sheet.autoSizeColumn(2);
-		
-		
+
 		wb.write(new FileOutputStream("exl.xlsx"));
 		wb.close();
-		
+
 		XSSFWorkbook wb1 = new XSSFWorkbook(new FileInputStream("exl.xlsx"));
 		XSSFSheet sheet1 = wb1.getSheetAt(0);
 		XSSFRow row1 = sheet1.getRow(0);
-		if(row1.getCell(0).getCellType() == XSSFCell.CELL_TYPE_STRING) {
-			System.out.println(row1.getCell(0).getStringCellValue());
-		}
-		
-
-		
+		System.out.println(row1.getCell(0).getStringCellValue());
+		System.out.println(row1.getCell(1).getDateCellValue());
+		System.out.println(row1.getCell(2).getStringCellValue());
 		
 
 	}
---
+++ src/excelops/illegalSheetIndexException.java
@@ -0,0 +1,10 @@
+package excelops;
+
+public class illegalSheetIndexException extends NullPointerException{
+
+	public illegalSheetIndexException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+
+}
